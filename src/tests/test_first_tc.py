16654ad70054cc034199ecc8e554e408b0af24b6
nguyenquangtay <49488470+nguyenquangtay@users.noreply.github.com>
2022-10-14 14:05:30 +0700

    'pytest_parameterize'

--- src/tests/test_first_tc.py
+++ src/tests/test_first_tc.py
@@ -1,3 +1,5 @@
+import pytest
+
 
 def test_add():
     assert 1 + 1 == 2
@@ -10,10 +12,25 @@ def test_multiply():
 def test_true_is_true():
     assert True is True
 
+
 # For exception case
 
 def test_divide_zero():
     try:
-        1/0
+        1 / 0
     except ZeroDivisionError as e:
         assert 'division by zero' == str(e)
+
+
+# parameterize example
+data = [
+    (1, 1, 2),
+    (2, 3, 5),
+    (-2, 0, -2),
+    (-1.1, 1.1, 0)
+]
+
+
+@pytest.mark.parametrize('num1, num2, result', data)
+def test_add_param(num1, num2, result):
+    assert num1 + num2 == result
