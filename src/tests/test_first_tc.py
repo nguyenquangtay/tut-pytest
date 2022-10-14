372ba8b12af4d8c79818d8b58a437f1361eb8a2c
nguyenquangtay <49488470+nguyenquangtay@users.noreply.github.com>
2022-10-14 12:24:42 +0700

    exception example

--- src/tests/test_first_tc.py
+++ src/tests/test_first_tc.py
@@ -9,3 +9,11 @@ def test_multiply():
 
 def test_true_is_true():
     assert True is True
+
+# For exception case
+
+def test_divide_zero():
+    try:
+        1/0
+    except ZeroDivisionError as e:
+        assert 'division by zero' == str(e)
